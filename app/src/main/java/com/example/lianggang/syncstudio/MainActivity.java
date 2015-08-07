e9b2f6b09441248699775b306a6127e651148aa2
zhulg <lg.json@gmail.com>
2015-08-07 09:53:19 +0800

    change test

--- app/src/main/java/com/example/lianggang/syncstudio/MainActivity.java
+++ app/src/main/java/com/example/lianggang/syncstudio/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lianggang.syncstudio;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("","====this is a test ");
     }
 
     @Override
